0467ce2d5e7b61bcaef8ca103275eb384aebccfe
bujosa <davidbujosa@gmail.com>
2023-11-12 04:21:37 -0400

    feat: implement logic for CRUD in controller

--- controller/user.go
+++ controller/user.go
@@ -1,39 +1,184 @@
 package controller
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"keid/common/functions"
+	"keid/common/types"
+	"keid/model"
+	"keid/repository/user"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
-type User struct{}
+type User struct {
+	Repository *user.UserRepository
+}
 
 func (u *User) GetAll(w http.ResponseWriter, r *http.Request) {
-	// do something
+	cursorStr := r.URL.Query().Get("cursor")
+	if cursorStr == "" {
+		cursorStr = "0"
+	}
+
+	const decimal = 10
+	const bitSize = 64
+	cursor, err := strconv.ParseUint(cursorStr, decimal, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	const size = 50
+	res, err := u.Repository.GetAll(r.Context(), types.GetAllPage{
+		Offset: cursor,
+		Size:   size,
+	})
+	if err != nil {
+		fmt.Println("failed to find all:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		Items []model.User `json:"items"`
+		Next  uint64       `json:"next,omitempty"`
+	}
+	response.Items = res.Users
+	response.Next = res.Cursor
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
 }
 
 func (u *User) GetById(w http.ResponseWriter, r *http.Request) {
-	// do something
+	idParam := chi.URLParam(r, "id")
+
+	userID := uuid.MustParse(idParam)
+
+	o, err := u.Repository.GetById(r.Context(), userID)
+	if errors.Is(err, user.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Println("failed to find by id:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(o); err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (u *User) Create(w http.ResponseWriter, r *http.Request) {
-	// do something
+	var body struct {
+		Name     string `json:"name"`
+		LastName string `json:"lastName"`
+		Email    string `json:"email"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	user := model.User{
+		ID:        uuid.New(),
+		Name:      body.Name,
+		LastName:  body.LastName,
+		Email:     body.Email,
+		CreatedAt: &now,
+		UpdatedAt: &now,
+	}
+
+	err := u.Repository.Create(r.Context(), user)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
 }
 
 func (u *User) Update(w http.ResponseWriter, r *http.Request) {
-	// do something
+	var body struct {
+		Name     string `json:"name"`
+		LastName string `json:"lastName"`
+		Email    string `json:"email"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	idParam := chi.URLParam(r, "id")
+
+	userID := uuid.MustParse(idParam)
+
+	res, err := u.Repository.GetById(r.Context(), userID)
+	if errors.Is(err, user.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Println("failed to find by id:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res = functions.UpdateFields(res, body).(model.User)
+
+	err = u.Repository.Update(r.Context(), res)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (u *User) Delete(w http.ResponseWriter, r *http.Request) {
-	// do something
-}
+	idParam := chi.URLParam(r, "id")
 
-func LoadUserRoutes(router chi.Router) {
-	userHandler := &User{}
+	userId := uuid.MustParse(idParam)
 
-	router.Get("/", userHandler.GetAll)
-	router.Post("/", userHandler.Create)
-	router.Get("/{id}", userHandler.GetById)
-	router.Put("/{id}", userHandler.Update)
-	router.Delete("/{id}", userHandler.Delete)
+	err := u.Repository.Delete(r.Context(), userId)
+	if errors.Is(err, user.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Println("failed to find by id:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }

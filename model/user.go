01226f3f815e0114334635b67e4fa3fa8dcf9ed9
bujosa <davidbujosa@gmail.com>
2023-11-12 03:11:09 -0400

    feat: add fields for user

--- model/user.go
+++ model/user.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID  `json:"id"`
+	Name      string     `json:"name"`
+	LastName  string     `json:"lastName"`
+	Email     string     `json:"email"`
+	CreatedAt *time.Time `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt"`
+}

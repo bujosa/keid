18adf275142aea3e0b0e7d2ea602c2d9462d2495
bujosa <davidbujosa@gmail.com>
2023-11-12 02:51:15 -0400

    feat: implement gracefully shutdown

--- app/app.go
+++ app/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -20,6 +21,9 @@ func New() *App {
 	}
 }
 
+// Start starts the application server.
+// It pings the Redis database and listens on port 8080 for incoming requests.
+// It returns an error if there was a problem starting the server.
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
 		Addr:    ":8080",
@@ -27,15 +31,34 @@ func (a *App) Start(ctx context.Context) error {
 	}
 
 	err := a.db.Ping(ctx).Err()
-
 	if err != nil {
 		return fmt.Errorf("failed to ping redis: %w", err)
 	}
+	defer func() {
+		if err := a.db.Close(); err != nil {
+			fmt.Printf("failed to close redis connection: %v\n", err)
+		}
+	}()
 
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to listen and serve: %w", err)
+	fmt.Println("Starting server on port 8080")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to listen and serve: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
-
-	return nil
 }
